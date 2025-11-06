a0bcb9090ddbd1bff6728ecbf1d20498146b4b0a
EloDrolet <elo_drolet@hotmail.com>
2025-11-06 11:37:45 -0500

    11.37

--- PrjTP2_AUT25/src/main/java/ca/qc/bdeb/sim/prjtp2_aut25/MainJavaFX.java
+++ PrjTP2_AUT25/src/main/java/ca/qc/bdeb/sim/prjtp2_aut25/MainJavaFX.java
@@ -3,12 +3,17 @@ package ca.qc.bdeb.sim.prjtp2_aut25;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainJavaFX extends Application {
+    public final static Canvas canva = new Canvas(900,580);
+    public final static double WIDTH = canva.getWidth();
+    public final static double HEIGHT = canva.getHeight();
     @Override
     public void start(Stage stage) throws IOException {
         var root = new Pane();
