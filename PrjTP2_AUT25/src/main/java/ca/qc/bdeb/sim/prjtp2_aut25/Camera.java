1be171e161611c000626eb725ff4bfdbe9f6c365
EloDrolet <elo_drolet@hotmail.com>
2025-11-12 14:09:29 -0500

    Update Camera.java

--- PrjTP2_AUT25/src/main/java/ca/qc/bdeb/sim/prjtp2_aut25/Camera.java
+++ PrjTP2_AUT25/src/main/java/ca/qc/bdeb/sim/prjtp2_aut25/Camera.java
@@ -1,4 +1,15 @@
 package ca.qc.bdeb.sim.prjtp2_aut25;
 
+import javafx.geometry.Point2D;
+
 public class Camera {
+    private Point2D positionCamera;
+
+    public Point2D coordoEcran(Point2D positionMonde) {
+        return positionMonde.subtract(positionCamera);
+    }
+    public void suivreCamelot(Camelot camelot){
+        positionCamera = new Point2D(camelot.position.getX(), positionCamera.getY());
+
+    }
 }
