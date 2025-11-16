566329986bd6467c8a090706908a9b3de4dde8e6
hsslm <hajjamiselma@outlook.com>
2025-11-16 15:41:51 -0500

    Ajout de la classe boite aux lettres

--- PrjTP2_AUT25/src/main/java/ca/qc/bdeb/sim/prjtp2_aut25/Maison.java
+++ PrjTP2_AUT25/src/main/java/ca/qc/bdeb/sim/prjtp2_aut25/Maison.java
@@ -4,14 +4,30 @@ import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
-public class Maison extends ObjetDuJeu{
-    public Maison() {
-        super(Point2D.ZERO,Point2D.ZERO,Point2D.ZERO,Point2D.ZERO);
+import java.util.Random;
+
+public class Maison extends ObjetDuJeu {
+    private int adresse;
+    private boolean abonneeAuJournal;
+    private BoiteAuxLettres boiteAuxLettres;
+    private Random random;
+
+    public int getAdresse() {
+        return adresse;
+    }
+
+    public boolean isAbonneeAuJournal() {
+        return abonneeAuJournal;
+    }
+
+    public Maison(int adresse) {
+        super(Point2D.ZERO, Point2D.ZERO, Point2D.ZERO, Point2D.ZERO);
+        this.adresse = adresse;
+        this.abonneeAuJournal = random.nextBoolean();
     }
 
     @Override
     public void draw(GraphicsContext context, Camera camera) {
-
 
 
     }
