565cb2d856b1a6a63187c22d11f14a4da02b9efe
EloDrolet <elo_drolet@hotmail.com>
2025-11-19 13:07:07 -0500

    bro

--- PrjTP2_AUT25/src/main/java/ca/qc/bdeb/sim/prjtp2_aut25/Decor.java
+++ PrjTP2_AUT25/src/main/java/ca/qc/bdeb/sim/prjtp2_aut25/Decor.java
@@ -3,12 +3,8 @@ package ca.qc.bdeb.sim.prjtp2_aut25;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
 
 public class Decor extends ObjetDuJeu {
-
-    private Point2D tailleBrique = new Point2D(192, 96);
-    private Image imageBrique = new Image("brique.png");
 
     public Decor() {
         super(Point2D.ZERO,
@@ -20,9 +16,10 @@ public class Decor extends ObjetDuJeu {
 
     @Override
     public void draw(GraphicsContext context, Camera camera) {
-        for (double y = position.getY(); y < MainJavaFX.HEIGHT; y += taille.getY()) {
-            for (double x = position.getX(); x < MainJavaFX.WIDTH; x += taille.getX()) {
-                context.drawImage(new Image("brique.png"), x, y);
+        var coordoCam = camera.coordoEcran(position);
+        for (double y = coordoCam.getY(); y < MainJavaFX.HEIGHT; y += taille.getY()) {
+            for (double x = coordoCam.getX(); x < MainJavaFX.WIDTH; x += taille.getX()) {
+                context.drawImage(ImageManager.getImage("brique.png"), x, y);
             }
         }
     }
---
+++ PrjTP2_AUT25/src/main/java/ca/qc/bdeb/sim/prjtp2_aut25/ImageManager.java
@@ -0,0 +1,21 @@
+package ca.qc.bdeb.sim.prjtp2_aut25;
+
+import javafx.scene.image.Image;
+
+import java.util.HashMap;
+
+public class ImageManager {
+    private static HashMap<String, Image> images = new HashMap<>();
+
+    public static Image getImage(String path) {
+
+        if(images.containsKey(path))
+            return images.get(path);
+
+        var image = new Image(path);
+
+        images.put(path, image);
+
+        return image;
+    }
+}
