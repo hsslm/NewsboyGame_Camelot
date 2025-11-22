57d4a537dea9d4c1ba93247d527948e2ebbea483
EloDrolet <elo_drolet@hotmail.com>
2025-11-22 16:16:58 -0500

    ajout barreAffichage

--- PrjTP2_AUT25/src/main/java/ca/qc/bdeb/sim/prjtp2_aut25/BarreAffichage.java
+++ PrjTP2_AUT25/src/main/java/ca/qc/bdeb/sim/prjtp2_aut25/BarreAffichage.java
@@ -1,7 +1,14 @@
+package ca.qc.bdeb.sim.prjtp2_aut25;
+
+import java.util.ArrayList;
+
 public class BarreAffichage {
     private ArrayList<Journal> journaux;
     private ArrayList<Integer> adresses;
     
+
+
+
 
 
 
