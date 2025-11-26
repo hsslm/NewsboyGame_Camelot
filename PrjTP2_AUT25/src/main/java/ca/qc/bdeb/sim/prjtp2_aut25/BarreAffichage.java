75d6a0b9f2308787c988742145176fa56d8e8014
hsslm <hajjamiselma@outlook.com>
2025-11-26 11:38:13 -0500

    Optimisation

--- PrjTP2_AUT25/src/main/java/ca/qc/bdeb/sim/prjtp2_aut25/BarreAffichage.java
+++ PrjTP2_AUT25/src/main/java/ca/qc/bdeb/sim/prjtp2_aut25/BarreAffichage.java
@@ -10,8 +10,8 @@ import java.util.ArrayList;
 
 public class BarreAffichage {
     private int nbJournaux;
-    private ArrayList<Integer> adresses;
     private int argent;
+    private ArrayList<Integer> adresses;
     private Image imgJournal;
     private Image imgArgent;
     private Image imgMaison;
@@ -24,10 +24,6 @@ public class BarreAffichage {
         this.imgJournal = ImageManager.getImage("icone-journal.png");
         this.imgArgent = ImageManager.getImage("icone-dollar.png");
         this.imgMaison = ImageManager.getImage("icone-maison.png");
-    }
-
-    public int getArgent() {
-        return argent;
     }
 
     public void setAdresses(ArrayList<Integer> adresses) {
@@ -48,32 +44,32 @@ public class BarreAffichage {
 
     public void draw(GraphicsContext context, Camera camera){
 
-         //dessin icone journal + nombre
+         //Icone de journal et quantité de journaux
+
         context.drawImage(imgJournal,5,8);
         context.setFill(Color.WHITE);
-        context.setFont(new Font("comic sans MS",25));
+        context.setFont(new Font("Comic sans MS",25));
         context.fillText(String.valueOf(nbJournaux),60,25);
-        //dessin icone argent + nombre
+
+        //Icone d'argent et quantité
+
         context.drawImage(imgArgent,80,10);
         context.fillText(String.valueOf(argent),140,25);
-        //dessin icone maison + adresses
+
+        //Icone de maison et adresses de livraison
         context.drawImage(imgMaison,155,8);
-        context.setFont(new Font("comic sans MS",20));
+        context.setFont(new Font("Comic sans MS",20));
         var index = 0;
         for(Integer i : adresses){
             context.fillText(String.valueOf(i),215+index,25);
             index+=45;
         }
 
+        //Fond gris et opacité
 
-        //dessin fond gris transparent
         Color grisTransparent = Color.gray(0.2,0.5);
         context.setFill(grisTransparent);
         context.fillRect(0, 0,MainJavaFX.WIDTH,50);
-
-
-
-
 
     }
 
